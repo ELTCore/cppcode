465b2850298af44ec0ab82081dc0e5fafa906a0e
Laniakea <57189118+ELTCore@users.noreply.github.com>
2022-06-24 23:09:21 +0800

    20220624

---
+++ leetcode/cpp240.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0)
+        {
+            if (matrix[x][y] == target)
+            {
+                return true;
+            }
+            if (matrix[x][y] > target)
+            {
+                --y;
+            }
+            else
+            {
+                ++x;
+            }
+        }
+        return false;
+    }
+};
+
+// Time spent: 100ms
+// Memory usage: 14.3MB
