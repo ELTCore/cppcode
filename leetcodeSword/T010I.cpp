0c260773ce733adee5fea69a7619dc6a07ae7a1a
Laniakea <57189118+ELTCore@users.noreply.github.com>
2023-02-03 22:21:37 +0800

    Update T010I.cpp

--- leetcodeSword/T010I.cpp
+++ leetcodeSword/T010I.cpp
@@ -9,6 +9,34 @@ class Solution
 public:
     int fib(int n)
     {
+        int MOD = 1000000007;
+
+        if (n < 2)
+        {
+            return n;
+        }
+
+        int p = 0, q = 0, r = 1;
+
+        for (int i = 2; i <= n; ++i)
+        {
+            p = q;
+            q = r;
+            r = (p + q) % MOD;
+        }
+
+        return r;
+    }
+
+private:
+    // overtime
+    int fib_prvt(int n)
+    {
+        if (n < 0)
+        {
+            return -1;
+        }
+
         if (n == 0)
         {
             return 0;
@@ -18,14 +46,14 @@ public:
             return 1;
         }
 
-        return fib(n - 1) + fib(n - 2);
+        return fib_prvt(n - 1) + fib_prvt(n - 2);
     }
 };
 
 int main(int argc, char const *argv[])
 {
 
-    cout << "0 + 1 + 2 + 3 + 5 = " << Solution().fib(5) << endl;
+    cout << "0 1 1 2 3 _ 8\nres:" << Solution().fib(5) << endl;
 
     system("pause");
 
