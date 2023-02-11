3cbd3d2858927f24897e24baf937b3dacf7a49e7
Laniakea <57189118+ELTCore@users.noreply.github.com>
2023-02-11 18:04:07 +0800

    Create T022.cpp

---
+++ leetcodeSword/T022.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *getKthFromEnd(ListNode *head, int k)
+    {
+        if (!head || !(head->next))
+        {
+            return head;
+        }
+
+        ListNode *p = head;
+        ListNode *q = head;
+
+        for (int i = 0; i < k; ++i)
+        {
+            if (q->next)
+            {
+                q = q->next;
+            }
+            else
+            {
+                return head;
+            }
+        }
+
+        while (q)
+        {
+            p = p->next;
+            q = q->next;
+        }
+    }
+};
