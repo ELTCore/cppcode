67295cc6b2204a98867612b6359dd6918043330f
Laniakea <57189118+ELTCore@users.noreply.github.com>
2023-02-25 14:24:23 +0800

    20230225

---
+++ leetcodeSword/T013.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int movingCount(int m, int n, int k)
+    {
+        if (k == 0)
+        {
+            return 1;
+        }
+
+        return -1;
+    }
+};
