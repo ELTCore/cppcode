640c934ee8ac59812caaec3a181487375fb0a5a7
Laniakea <57189118+ELTCore@users.noreply.github.com>
2023-03-04 16:54:03 +0800

    Create T036.cpp

---
+++ leetcodeSword/T036.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node *_left, Node *_right)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+
+class Solution
+{
+public:
+    Node *treeToDoublyList(Node *root)
+    {
+    }
+};
